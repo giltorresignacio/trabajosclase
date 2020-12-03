82b8de5bfeaff7022dad05b423a4aa0e8eea1c83
Tardes <tardes@iMac-de-CAS.local>
2020-12-03 17:43:21 +0100

    v1

--- main.js
+++ main.js
@@ -13,15 +13,15 @@ function informarMayoriaEdad() {
     console.log("ha tocado el botón")
     //recoger valor caja de texto  
     var caja_edad = document.getElementById("edad");//let 
-    console.log("El id de la caja es" +caja_edad.id);//concatenar
-    console.log("El valor de la caja es" +caja_edad.value);//concatenar
+    console.log("El id de la caja es" + " " +caja_edad.id);//concatenar
+    console.log("El valor de la caja es" + " " +caja_edad.value);//concatenar
     var edad= caja_edad.value;//obtengo el valor
     //validar si es un nº o palabra
     //si es texto 
         //error entrada
     //si es nº  
         //comparar con limite de edad 
-        if (edad < MAYORIA_DE_EDAD)
+        if (edad >=MAYORIA_DE_EDAD)
         { 
             console.log("Es Mayor de edad"); 
         }else
