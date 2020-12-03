a46d969144d7d1c1c277af85d2a208bc2e1fd523
Tardes <tardes@iMac-de-CAS.local>
2020-12-03 16:41:03 +0100

    ficheros de ayuda

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function mayoriaEdad () {
 
 a() */
 const MAYORIA_DE_EDAD = 18;
-function informarMayoriaEdad() {}
+function informarMayoriaEdad() {
     console.log("ha tocado el botón")
     //recoger valor caja de texto  
     var caja_edad = document.getElementById("edad");//let 
@@ -21,11 +21,12 @@ function informarMayoriaEdad() {}
         //error entrada
     //si es nº  
         //comparar con limite de edad 
-        if (edad >= MAYORIA_DE_EDAD)
+        if (edad < MAYORIA_DE_EDAD)
         { 
-            console.log("Es Mayor de edad")
-        }
+            console.log("Es Mayor de edad"); 
+        }else
         {
             console.log("Es Menor de edad")
         }
         //si es mayor indicarlo y si es menor informar menor
+    }
